df09df8d74699478dfad3b23064e03af599b7fde
Darab <darabcode@gmail.com>
2022-08-11 19:08:45 +0100

    Updated the script page

--- script.js
+++ script.js
@@ -2,14 +2,42 @@ const answer = document.querySelectorAll(".answer");
 
 const quest_card = document.querySelectorAll(".quest-card");
 
-for (let i = 0; i <= quest_card.length; i++) {
-  for (let k = 0; k < answer.length; k++) {
-    quest_card[i].addEventListener("click", function () {
-      if (answer[k].style.display === "none") {
-        answer[k].style.display = "block";
-      } else {
-        answer[k].style.display = "none";
-      }
-    });
-  }
+function showAns() {
+  quest_card[0].addEventListener("click", () => {
+    if (answer[0].style.display === "none") {
+      answer[0].style.display = "block";
+    } else {
+      answer[0].style.display = "none";
+    }
+  });
+  quest_card[1].addEventListener("click", () => {
+    if (answer[1].style.display === "none") {
+      answer[1].style.display = "block";
+    } else {
+      answer[1].style.display = "none";
+    }
+  });
+  quest_card[2].addEventListener("click", () => {
+    if (answer[2].style.display === "none") {
+      answer[2].style.display = "block";
+    } else {
+      answer[2].style.display = "none";
+    }
+  });
+  quest_card[3].addEventListener("click", () => {
+    if (answer[3].style.display === "none") {
+      answer[3].style.display = "block";
+    } else {
+      answer[3].style.display = "none";
+    }
+  });
+  quest_card[4].addEventListener("click", () => {
+    if (answer[4].style.display === "none") {
+      answer[4].style.display = "block";
+    } else {
+      answer[4].style.display = "none";
+    }
+  });
 }
+
+showAns();
